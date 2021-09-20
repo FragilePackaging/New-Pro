9f781ba5a1c558139f609bf3daba4f685923ee28
FragilePackaging <86084817+FragilePackaging@users.noreply.github.com>
2021-09-20 19:44:36 -0400

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,18 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+getCount(){
+  var playerCountRef=database.ref("playerCount")
+  playerCountRef.on("value",(data)=>{playerCount=data.var()})
+}
+updateCount(count){
+  database.ref("/").update({
+    playerCount:count
+  })
+}
+ }
+
